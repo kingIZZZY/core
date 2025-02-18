a3faa10f2776403ab5fc995be40dbc5bacf600f0
Albert Chen <albert.cht@gmail.com>
2025-02-18 22:14:23 +0800

    chore: adjust context namespace

---
+++ src/Context/ApplicationContext.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelHyperf\Context;
+
+use Hyperf\Context\ApplicationContext as HyperfApplicationContext;
+use LaravelHyperf\Container\Contracts\Container as ContainerContract;
+use TypeError;
+
+class ApplicationContext extends HyperfApplicationContext
+{
+    /**
+     * @throws TypeError
+     */
+    public static function getContainer(): ContainerContract
+    {
+        /* @phpstan-ignore-next-line */
+        return self::$container;
+    }
+}
